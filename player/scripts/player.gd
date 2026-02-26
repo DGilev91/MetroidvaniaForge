d38f1677acacced53f7a14236b9d83d0441948e3
DimaG <dimagilev91@gmail.com>
2026-02-26 21:21:55 +0500

    state machine

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -1,14 +1,36 @@
 class_name  Player extends CharacterBody2D
 
+@onready var states_node: Node = $States
+
+#region //State Machine Variables
+var states: Array[PlayerState]
+var current_state: PlayerState:
+	get: return states.front()
+var previous_state: PlayerState:
+	get: return states[1]
+#endregion
+
+#region /// standart variables
+var direction: Vector2 = Vector2.ZERO
+var gravity: float = 980
+#endregion
+
+
+func _ready() -> void:
+	initialize_states()
+
 func _process(_delta: float) -> void:
 	pass
 
 func _physics_process(_delta: float) -> void:	
-	velocity.x = 0
-	if Input.is_action_pressed("LEFT"):
-		velocity.x = -100
-	elif Input.is_action_pressed("RIGHT"):
-		velocity.x = 100
-	
-	velocity.y = velocity.y + 980 * _delta
-	move_and_slide()
+	pass
+
+func initialize_states() -> void:
+	states = []
+	for n in states_node.get_children():
+		if n is PlayerState:
+			n.player = self
+			states.append(n)
+			
+	for state in states:
+		state.init()
---
+++ player/states/00_player_stale.gd
@@ -0,0 +1,28 @@
+@icon("res://player/states/state.svg")
+class_name PlayerState extends Node
+
+
+var player: Player
+var next_state: PlayerState
+
+#region /// state references
+# references to all states
+#endregion
+
+func init() -> void:
+	pass
+	
+func enter() -> void:
+	pass
+	
+func exit() -> void:
+	pass
+	
+func handle_inpud(_event: InputEvent) -> PlayerState:
+	return next_state
+
+func process(_delta: float) -> PlayerState:
+	return next_state
+	
+func physics_process(delta: float) -> PlayerState:
+	return next_state
